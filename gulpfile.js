47a5df67eae2c07dbc7fe168792ec4926ba9063b
Sebastian Gomez A <seagomezar@MacBook-Pro-de-Sebastian.local>
2016-06-10 17:56:39 -0500

    Implementing Tests Tasks for vscode

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
- return gulp.src('./sass/**/*.scss')
-   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-   .pipe(gulp.dest('./app/css'));
+var Server = require('karma').Server;
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: false
+  }, done).start();
 });
- 
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'));
+});
+
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
