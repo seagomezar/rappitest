82a6d615e6c174423fe04a609a34297cb5424d1b
Sebastian Gomez A <seagomezar@MacBook-Pro-de-Sebastian.local>
2016-06-13 15:49:58 -0500

    Creating deploy

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var Server = require('karma').Server;
+var usemin = require('gulp-usemin');
+var minifyCSS = require('gulp-cssnano');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
 
 /**
  * Run test once and exit
@@ -22,4 +26,14 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('main', function() {
+  gulp.src(['app/index.html'])
+    .pipe(usemin({
+      Stylescss: [minifyCSS({zindex: false}), 'concat'],
+      Scriptsjs: [ngAnnotate(), uglify(), 'concat'],
+      Vendorjs: [ngAnnotate(), uglify(), 'concat']
+    }))
+    .pipe(gulp.dest('dist'));
 });
